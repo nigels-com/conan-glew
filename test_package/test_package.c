46b0b7e49809e8af265df4b63cb5a660c4f82462
Nigel Stewart <nigels@users.sourceforge.net>
2018-10-14 18:01:24 +1000

    Use GLUT for testing GLEW at runtime

--- test_package/test_package.c
+++ test_package/test_package.c
@@ -1,6 +1,33 @@
-#include "GL/glew.h"
+#include <GL/glew.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
 
-int main () {
-  glewGetString(GLEW_VERSION);
-  return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char * argv[])
+{
+	GLenum err;
+
+#if defined(WIN32) || defined(__APPLE__)
+    if (1)
+#else
+    /* Need X11 display to init GLUT */
+    if (getenv("DISPLAY")!=NULL)
+#endif
+    {
+        glutInit(&argc, argv);
+        glutCreateWindow("GLUT Window");
+        err = glewInit();
+        if (GLEW_OK != err)
+        {
+            printf("Error: %s\n", glewGetErrorString(err));
+            return EXIT_FAILURE;
+        }
+    }
+    printf("Bincrafters GLEW %s\n", glewGetString(GLEW_VERSION));
+    return EXIT_SUCCESS;
 }
